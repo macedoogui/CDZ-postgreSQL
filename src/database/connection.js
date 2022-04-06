685da7d6010bbf0f162b6eb02b2f0d34bab079cf
Guilherme <macedoogui@gmail.com>
2022-04-05 23:06:24 -0300

    update heroku

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,6 +3,17 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
+/*export const connection = new Sequelize(
+    process.env.DB_BASE,
+    process.env.DB_USER,
+    process.env.DB_PASS,    
+    {
+    host: process.env.DB_HOST,
+    port:5432,
+    dialect:'postgres'
+    }
+)*/
+
 export const connection = new Sequelize(
     process.env.DATABASE_URL, 
     {
